fd4f0a589388953a5febd6ad475fc78fa53c33a7
Tom O <58118455+Tatooine-Soldier@users.noreply.github.com>
2022-10-11 22:32:15 +0100

    Add files via upload

--- main.go
+++ main.go
@@ -2,20 +2,30 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func getRoot(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the homepage")
+	_, err := fmt.Fprintf(w, "Welcome to the homepage")
+	checkErr(err)
 }
 func getHello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "hello")
+	_, err := fmt.Fprintf(w, "hello")
+	checkErr(err)
+}
 
+func checkErr(err error) {
+	if err != nil {
+		log.Panic(err)
+	}
 }
 
 func main() {
-	http.HandleFunc("/", getRoot)
+	// http.HandleFunc("/", getRoot)
 	http.HandleFunc("/hello", getHello)
+	fs := http.FileServer(http.Dir("../../dist"))
+	http.Handle("/", fs)
 
 	err := http.ListenAndServe(":3333", nil)
 	fmt.Printf("%v", err)
