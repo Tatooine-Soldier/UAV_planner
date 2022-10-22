2a6056ead1643c0cbdd51e89b404c28e4b96d529
Tom O <58118455+Tatooine-Soldier@users.noreply.github.com>
2022-10-22 12:44:00 +0100

    Handle flight requirements data

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/gob"
 	"fmt"
 	"log"
 	"net/http"
+	"text/template"
+
+	"crypto/sha256"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -12,6 +17,18 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
+
+type Head struct {
+	ContentType string
+	Status      int
+}
+
+type HTML struct {
+	Head      Head
+	Name      string
+	Templates *template.Template
+	// contains filtered or unexported fields
+}
 
 func getRoot(w http.ResponseWriter, r *http.Request) {
 	_, err := fmt.Fprintf(w, "Welcome to the homepage")
@@ -40,9 +57,9 @@ func connectDB() {
 }
 
 //InsertDB(context.TODO(), client)
-func insertDB(ctx context.Context, client *mongo.Client, user primitive.D) {
+func insertDB(ctx context.Context, client *mongo.Client, user primitive.D, collection string) {
 	fmt.Printf("\nINSERTING %v\n", user)
-	usersCollection := client.Database("fyp_test").Collection("test")
+	usersCollection := client.Database("fyp_test").Collection(collection)
 	result, err := usersCollection.InsertOne(ctx, user)
 	if err != nil {
 		fmt.Printf("%v", err)
@@ -56,17 +73,110 @@ func loginRequest(w http.ResponseWriter, r *http.Request) {
 		panic("GET method not permitted")
 	} else {
 		r.ParseForm()
-		fmt.Printf("%v %v", r.Form["username"], r.Form["password"])
+		fmt.Printf("BEFORE HASH AND STORAGE--> %v %v", r.Form["username"], r.Form["password"])
 	}
 	username := r.Form["username"]
 	password := r.Form["password"]
-	//add checks before calling insert
 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://localhost:27017"))
 	if err != nil {
 		panic(err)
 	}
-	user := bson.D{{"fullName", username}, {"age", password}}
-	insertDB(context.TODO(), client, user)
+
+	itemByted := encodeToByte(password)
+
+	hashedVal := sha256.New()
+	hashedVal.Write([]byte(itemByted))
+
+	user := bson.D{{"fullName", username}, {"password", hashedVal.Sum(nil)}}
+	// checkDB(context.TODO(), client, user)
+	insertDB(context.TODO(), client, user, "users")
+}
+
+// encode the string array into byte array
+func encodeToByte(pw []string) []byte {
+	buf := &bytes.Buffer{}
+	bytedPw := buf.Bytes()
+	if len(pw) != 0 {
+		gob.NewEncoder(buf).Encode(pw)
+		bytedPw = buf.Bytes()
+		return bytedPw
+	}
+	return bytedPw
+}
+
+// func checkDB(ctx context.Context, client *mongo.Client, user primitive.D) {
+// }
+
+func locationRequest(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		panic("GET method not permitted")
+	} else {
+		r.ParseForm()
+		fmt.Printf("BEFORE HASH AND STORAGE--> %v %v", r.Form["latitude"], r.Form["longitude"])
+	}
+
+	latitude := r.Form["latitude"]
+	longitude := r.Form["longitude"]
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		panic(err)
+	}
+
+	user := bson.D{{"latitude", latitude}, {"longitude", longitude}}
+	// checkDB(context.TODO(), client, user)
+	insertDB(context.TODO(), client, user, "coordinates")
+}
+
+func speedRequest(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		panic("GET method not permitted")
+	} else {
+		r.ParseForm()
+		fmt.Printf("BEFORE HASH AND STORAGE--> %v %v %v", r.Form["lowspeed"], r.Form["midspeed"], r.Form["highspeed"])
+	}
+
+	lowspeed := r.Form["lowspeed"]
+	midspeed := r.Form["midspeed"]
+	highspeed := r.Form["highspeed"]
+
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		panic(err)
+	}
+
+	user := bson.D{{"lowspeed", lowspeed}, {"midspeed", midspeed}, {"highspeed", highspeed}}
+	// checkDB(context.TODO(), client, user)
+	insertDB(context.TODO(), client, user, "speeds")
+}
+
+func totalRequest(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		panic("GET method not permitted")
+	} else {
+		r.ParseForm()
+		fmt.Printf("BEFORE HASH AND STORAGE-->%v %v %v %v %v %v %v", r.Form["latitude"], r.Form["longitude"], r.Form["lowspeed"], r.Form["highspeed"], r.Form["date"], r.Form["hour"], r.Form["minute"])
+	}
+
+	latitude := r.Form["latitude"]
+	longitude := r.Form["longitude"]
+	date := r.Form["date"]
+	hour := r.Form["hour"]
+	minute := r.Form["minute"]
+
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		panic(err)
+	}
+
+	user := bson.D{{"latitude", latitude}, {"longitude", longitude}, {"date", date}, {"hour", hour}, {"minute", minute}}
+	insertDB(context.TODO(), client, user, "total")
+	http.Redirect(w, r, "/planReview", http.StatusSeeOther)
+}
+
+func renderTmpl(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("rendering")
+	// render := render.New()
+	// r.HTML(w, http.StatusOK, "example", nil)
 }
 
 func main() {
@@ -75,6 +185,10 @@ func main() {
 	fs := http.FileServer(http.Dir("../../../dist"))
 	http.Handle("/", fs)
 	http.HandleFunc("/login", loginRequest)
+	http.HandleFunc("/location", locationRequest)
+	http.HandleFunc("/speed", speedRequest)
+	http.HandleFunc("/detailsSubmit", totalRequest)
+	http.HandleFunc("/planReview", renderTmpl)
 
 	listenerErr := http.ListenAndServe(":3333", nil)
 	fmt.Printf("%v", listenerErr)
