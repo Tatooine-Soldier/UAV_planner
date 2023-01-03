561f7e67f46e6e1989b9a1a64db9c9b413e9cceb
Tom O <58118455+Tatooine-Soldier@users.noreply.github.com>
2023-01-03 13:45:27 +0000

    Add files via upload

--- PlanFlightComponent.vue
+++ PlanFlightComponent.vue
@@ -1,9 +1,32 @@
+<script setup>
+  import MyGoogleMapComponent from "../components/MyGoogleMapComponent.vue"
+</script>
 
 <template>
-    <section class="flight-planner">
+    <section class="flight-planner" id="flight-planner">
         <!-- <form action="/planner" method="post"> -->
         <form @submit.prevent="handleSubmit()">
-        <section class="flight-planner-columns">
+            <section class="flight-details-container" id="flight-details-container">
+                <section class="flight-details">
+                    <section class="flight-details-content">
+                        <h1>Confirm Flight Details</h1>
+                        <section class="flight-details-content-grid">
+                            <div>Date: {{ date.day }}</div>
+                            <div>Time: {{ date.hour }}:{{ date.minute }}</div>
+                            <div>Speed: {{ speed }} </div>
+                            <div>Calculated distance: {{ distance }} KM</div>
+                        </section>
+                        <section class="flight-details-buttons">
+                            <input id="cancel-but" name="but" type="button" value="Cancel" v-on:click="disappear()"/>
+                            <button id="confirm-but">Confirm</button>
+                        </section>
+                    </section>
+                </section>
+            </section>
+            <section id="details-map-container">
+                <MyGoogleMapComponent></MyGoogleMapComponent>
+            </section>
+        <section class="flight-planner-columns" id="flight-planner-columns">
             <section class="fp-info-container">
                         <h1 id="fp-headers">When</h1>
                         <section class="fp-sub-info">
@@ -72,7 +95,7 @@
                                 <p><b id="or">OR</b></p>
                                 <p>Select location on the map below</p>
                                 <!-- <section></section> Display the map her-->
-                                
+                                <input id="map-but" name="but" type="button" value="MAP" v-on:click="showMap()"/>  
                             </section>
                         </section>
                     </section>
@@ -86,15 +109,15 @@
                                     <section class="speeds-option">
                                         <section class="speeds-inputs">
                                             <label for="lowspeed">Low: </label>
-                                            <input type="radio" name="lowspeed" value="lowspeed"  />
+                                            <input type="radio" name="speed" value="lowspeed"  ref="speed" />
                                         </section>
                                         <section class="speeds-inputs">
                                             <label for="midspeed">Mid: </label>
-                                            <input type="radio" name="midspeed" value="midspeed"  />
+                                            <input type="radio" name="speed" value="midspeed"  ref="speed" />
                                         </section>
                                         <section class="speeds-inputs">
                                             <label for="highspeed">High: </label>
-                                            <input type="radio" name="highspeed" value="highspeed" />
+                                            <input type="radio" name="speed" value="highspeed" ref="speed" />
                                         </section>
                                     </section>
                                     
@@ -106,12 +129,13 @@
                     <section class="fp-info-container">
                         <h1 id="fp-headers">Book</h1>
                         <section class="fp-sub-info">
-                            <p class="fp-subtitle">Request flight plan</p>
-                            <input id="submit" name="submit" type="submit" value="Add"/>
-                            {{ info }}
+                                <p class="fp-subtitle">Request flight plan</p>
+                                <input id="submit" name="submit" type="submit" value="Add"/>
+                                
                         </section>
                     </section>
         </section>
+        
     </form>
     </section>
 </template>
@@ -133,8 +157,6 @@
         border-right: solid 2px white;
         padding-bottom: 5px;
     }
-
-    
 
     .fp-info-container h1 {
         margin: 0px;
@@ -169,8 +191,21 @@
         padding: 5px 0px;
     }
 
-    button {
-        margin-top: 10%;;
+    #cancel-but {
+        padding: 5px;
+        width: 100%;
+    }
+
+    #confirm-but {
+        margin-left: 5px;
+        padding: 5px;
+        width: 100%;
+    }
+
+    #map-container {
+        display: none;
+        z-index: 1;
+        position: relative;
     }
 
     .coords-inputs {
@@ -197,12 +232,68 @@
         padding: 10px;
     }
 
+    .flight-details-container {
+        display: none;
+        z-index: 1;
+        position: relative;
+        width: 100%;
+    }
 
+    .flight-details {
+        border: solid 2px rgb(184, 255, 36); 
+        background-color: rgb(77, 76, 76);
+        width: 45%;
+        position: absolute;
+        padding: 10px;
+        margin-top: 2%;
+        margin-left: 26.5%;
+        
+        color: white;
+    }
+
+    .flight-details-content {
+        padding: 10px;
+    }
+
+    .flight-details-content h1 {
+        font-size: large;
+        margin-top: 0px;
+        margin-bottom: 0px;
+        text-align: center;
+    }
+
+    .flight-details-content-grid {
+        display: grid;
+        grid-template-rows: 50% 50%;
+        grid-template-rows: 25% 25% 25% 25%;
+        margin: 0% 5%;
+    }
+
+    .flight-details-buttons {
+        display: grid;
+        grid-template-columns: 50% 50%;
+        float: right;
+    }
+
+    .flight-details-buttons button:first-of-type{
+        margin-right: 5px;
+    }
+
+    #details-map-container {
+        position: absolute;
+        display: none;
+        z-index: 1;
+        left: 10%;
+        right: 10%;
+        background-color: grey;
+        border: solid 1px grey;
+
+    }
   
 
 </style>
 
-
+<!-- <script defer src="<https://maps.googleapis.com/maps/api/js?key=AIzaSyDTNOMjJP2zMMEHcGy2wMNae1JnHkGVvn0&callback=initMap>"> -->
 <script>
   import * as geolib from 'geolib';
 
@@ -218,12 +309,9 @@ export default {
             longitude: '',
             latitude: ''
         },
-        speed: {
-            lowspeed: '',
-            midspeed: '',
-            highspeed: ''
-        },
+        speed: null,
         info: null,
+        distance: null
       }
     },
   
@@ -235,18 +323,59 @@ export default {
         console.log(this.coords)
         var long = parseInt(this.coords.longitude)
         var lat = parseInt(this.coords.latitude)
-        console.log(
-            'DISTANCE-->',
-            geolib.getDistance({latitude: 0, longitude:0}, {
+        this.distance = geolib.getDistance({latitude: 0, longitude:0}, {
                     latitude: lat,
                     longitude: long,
-                }),
+                })
+        this.distance = geolib.convertDistance(this.distance, 'km');
+        console.log(
+            'DISTANCE-->',
+             this.distance,
             '<--DISTANCE'
         )
+        var details = document.getElementById('flight-details-container');
+        details.style.display = 'block';
+
+        this.speed = this.$refs.speed.value;
+    
+
+        var grey = document.getElementById('flight-planner-columns');
+        grey.style.opacity = 0.2;
+        grey.style.pointerEvents = "none";
+
+        
+      },
+      disappear: function (event) {
+        var details = document.getElementById('flight-details-container');
+        details.style.display = 'None';
+        console.log(event)
+
+        var grey = document.getElementById('flight-planner-columns');
+        grey.style.opacity = 1;
+        grey.style.pointerEvents = "initial";
       },
       handleSubmitCoords() {
         console.log(this.coords)
-      }
+      }, 
+      showMap(event) {
+            var map = document.getElementById("details-map-container")
+            map.style.display = "block"
+            console.log(event);
+      },
+    //   initMap() {
+    //     var options = {     
+    //         zoom: 10,
+    //         center: { lat: 33.933241, lng: -84.340288 }
+    //     }
+    //     var mapContainer = document.getElementById('map-container');
+    //     var myMap = new google.maps.Map(mapContainer, options);
+    //     var marker = new google.maps.Marker({
+    //         position: { lat: 33.933241, lng: -84.340288 },
+    //         map: myMap
+    //     });
+    //     console.log(marker);
+    //   }
+    }
     // }, mounted() {
 
     // fetch('http://localhost:3333', {
@@ -256,5 +385,8 @@ export default {
     // .then(data => this.info = data.message, console.log(this.info))
     // .catch(err => console.log(err.message))
 }
-}
+
+
 </script>
+
+
