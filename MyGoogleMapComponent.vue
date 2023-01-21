db09f618c8477f6e44b9ff28b5b3e6d5c73d0b66
Tom O <58118455+Tatooine-Soldier@users.noreply.github.com>
2023-01-21 19:32:32 +0000

    Dest/source markers connected to head/tail of waypoint path immediately

--- MyGoogleMapComponent.vue
+++ MyGoogleMapComponent.vue
@@ -5,6 +5,7 @@ import { Loader } from '@googlemaps/js-api-loader'
     import { computed, ref, onMounted, onUnmounted, watch } from 'vue'
     import { useGeolocation } from '../useGeolocation'
     import { airports } from '../airports'
+    import { LinkedList, Node } from '../linkedList'
     // import haversineDistance from './calculateDistance'
     const GOOGLE_MAPS_API_KEY = 'AIzaSyDTNOMjJP2zMMEHcGy2wMNae1JnHkGVvn0'
     export default {
@@ -18,24 +19,35 @@ import { Loader } from '@googlemaps/js-api-loader'
         
         const currPos = ref(null)
         const otherLoc = ref(null)
+        const waypointLoc = ref(null)
         let clickListener = null;
         let dragListener = null;
+        let waypointDragListener = null;
+        let waypointListener = null;
         let destDragListener = null;
         let cursor = {lat: 0.0, lng: 0.0}
-        
+        let oldMarkers = [];
 
         const loader = new Loader({ apiKey: GOOGLE_MAPS_API_KEY})
         const mapDivHere = ref(null);
       
         let sourceMarker = ref(null)
         let destMarker = ref(null)
-  
+        let waypoint = ref(null)
+        let oldMarker = ref(null)
+        let waypointMarker = ref(null)
+        var waypointDiv;
+        var waypointList = new LinkedList(sourceMarker, destMarker);
+        let wl = [];
+        var flightPlanCoordinates = [];
+
         let map = ref(null)
         
         onMounted(async () => {
           await loader.load()
           currPos.value = {lat: initial.value.lat, lng: initial.value.lng}
           console.log(currPos.value)
+          var circleList = [];
           map.value = new google.maps.Map(mapDivHere.value, {
             center: currPos.value,
             zoom: 9
@@ -60,10 +72,66 @@ import { Loader } from '@googlemaps/js-api-loader'
                   var c = document.getElementById('destCursorLng')
                   c.innerHTML = otherLoc.value.lng;
                 }
-              )
+              ),
+              displayWaypointClicker(),
+               waypointList = new LinkedList(sourceMarker.value, destMarker.value)
             )
           )
-        
+
+          waypointDiv = document.getElementById("addWaypoint");
+          waypointListener = waypointDiv.addEventListener("click", function(){
+            waypointLoc.value = {lat: map.value.center.lat(), lng: map.value.center.lng()}
+            flightPlanCoordinates.push(waypointLoc.value)
+            drawPath()
+            console.log("flightPlanCoordinates::::",flightPlanCoordinates)
+            waypointMarker.value = new google.maps.Marker({
+              map: map.value,
+              position: waypointLoc.value,
+              draggable: true,
+              icon: {
+                url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"
+              }
+            }),
+            waypointDragListener = waypointMarker.value.addListener(
+              'drag',
+              function(e) {
+                waypointLoc.value = {lat: e.latLng.lat(), lng: e.latLng.lng()}
+                console.log("dragging waypoint", waypointLoc.value)
+                updatePath( )
+                //flightPlanCoordinates.push(waypointLoc.value)
+                
+              },
+              
+              console.log("After dragging", flightPlanCoordinates)
+            )
+
+          
+            function drawPath() {
+                const flightPath = new google.maps.Polyline({
+                path: flightPlanCoordinates,
+                geodesic: true,
+                strokeColor: "#FF0000",
+                strokeOpacity: 0.5,
+                strokeWeight: 3,
+                });
+                flightPath.setMap(map.value) 
+                return flightPath
+              }
+            
+            function updatePath() {
+
+            }
+
+     
+            //   if (waypointLinesD) waypointLinesD.setMap(null)
+            // waypointLinesD = new google.maps.Polyline({
+            //   path: [otherLoc.value, flightPlanCoordinates[flightPlanCoordinates.length-1]],
+            //   map: map.value,
+            //   strokeColor: "#FF0000",
+            //   strokeOpacity: 0.5,
+            // })
+            console.log(flightPlanCoordinates)
+          })
 
           sourceMarker.value = new google.maps.Marker({
             position: currPos.value,
@@ -85,11 +153,20 @@ import { Loader } from '@googlemaps/js-api-loader'
               d.innerHTML = cursor.lat;
               var c = document.getElementById('cursorLng')
               c.innerHTML = cursor.lng;
+              var circle;
+              // if (circleList.length !== 0) {
+              //   for (circle in circleList) {
+              //     var t = check(cursor, circleList[circle], circleList[circle].radius)
+              //     console.log(t)
+              //     if (t) {
+              //       alert("IN")
+              //       return 
+              //     }
+              //   }
+              // }
               // this.$emit('changeLocation',cursor);
             }
           )
-
-          console.log("AIRPORTS-->",airports)
 
 
           for (const airport in airports) {
@@ -112,20 +189,36 @@ import { Loader } from '@googlemaps/js-api-loader'
                 a.style.display="none"; //fade out would be nice here
               },3200)
             })
-
+            circleList.push(cityCircle);
           }
           //do smaller airports next, 10 nautical mile radius, the 3 larger airports hav an extended area outside area already given so add it in
           //add onclick listener to the circle
+         
+          function check(marker, circle, radius) 
+          {  
+              var km = radius/1000;
+              var kx = Math.cos(Math.PI * circle.lat / 180) * 111;
+              var dx = Math.abs(circle.center.lng() - marker.lng) * kx;
+              var dy = Math.abs(circle.center.lat() - marker.lat) * 111;  
+              return Math.sqrt(dx * dx + dy * dy) <= km;
+          }
 
+          function displayWaypointClicker() {
+            var t = document.getElementById("addWaypoint")
+            t.style.display = "block";
+          }
+
+          
         
         })
         onUnmounted(async () => {
             if (clickListener) clickListener.remove()
             if (dragListener) dragListener.remove()
             if (destDragListener) destDragListener.remove()
+            if (waypointDragListener) waypointDragListener.remove()
         
         })
-        let line = null
+        let line = null //source to dest
         watch([map, currPos, otherLoc], () => {
           if (line) line.setMap(null)
           if (map.value && otherLoc.value != null)
@@ -136,11 +229,84 @@ import { Loader } from '@googlemaps/js-api-loader'
           
         })
 
-      //   const flightPathCoordinates = [
-      //   { lat: currPos.value.lat+2, lng: currPos.value.lng-1 },
-      //   { lat: currPos.value.lat+5, lng: currPos.value.lng-1.7 },
-      //   { lat: currPos.value.lat+10, lng: currPos.value.lng-2.3 }
-      // ];        
+        
+
+        let waypointLines = null //source to waypoint
+        watch([map,currPos, waypointLoc], () => {
+          if (waypointLines) waypointLines.setMap(null)
+            waypointLines = new google.maps.Polyline({
+              path: [currPos.value, flightPlanCoordinates[0]],
+              map: map.value,
+              strokeColor: "#FF0000",
+              strokeOpacity: 0.5,
+            })
+        })
+        // flightPlanCoordinates[flightPlanCoordinates.length-1]
+        let waypointLinesD = null  //waypoint to dest
+        watch([map,otherLoc, waypointLoc], () => {
+          if (waypointLinesD) waypointLinesD.setMap(null)
+            waypointLinesD = new google.maps.Polyline({
+              path: [otherLoc.value, flightPlanCoordinates[flightPlanCoordinates.length-1]],
+              map: map.value,
+              strokeColor: "#FF0000",
+              strokeOpacity: 0.5,
+            })
+        }) 
+
+       
+
+        
+
+
+        // let waypointLineZ = null   //between final marker in array and destMarker
+        // watch([map,otherLoc, wl[wl.length-1]], () => {
+        //   if (waypointLineZ) waypointLineZ.setMap(null)
+        //   waypointLineZ = new new google.maps.Polyline({
+        //       path: [otherLoc.value, wl[wl.length-1]],
+        //       map: map.value,
+        //       strokeColor: "#FF0000",
+        //       strokeOpacity: 0.5,
+        //     })
+        // })
+
+        // let waypointLineA = null   //between final marker in array and destMarker
+        // watch([map, currPos, wl[0]], () => {
+        //   if (waypointLineA) waypointLineA.setMap(null)
+        //   waypointLineA = new google.maps.Polyline({
+        //       path: [currPos.value, wl[0]],
+        //       map: map.value,
+        //       strokeColor: "#FF0000",
+        //       strokeOpacity: 0.5,
+        //     })
+        // })
+
+
+        // let waypointPath = null 
+        // watch([map, wl[0], wl[wl.length-1]], () => {
+        //   if (waypointPath) waypointPath.setMap(null)
+        //   waypointPath = new google.maps.Polyline({
+        //       path: wayPositionsList,
+        //       map: map.value,
+        //       strokeColor: "#FF0000",
+        //       strokeOpacity: 0.5,
+        //     })
+        // })
+
+        // var wayPositionsList = function() {
+        //   var item
+        //   var markerList = []
+        //   for (item in wl) {
+        //     markerList.push(wl[item].value.position)
+        //   } 
+        //   return markerList
+        // }
+
+        
+        // const flightPathCoordinates = [
+        //   { lat: currPos.value.lat+2, lng: currPos.value.lng-1 },
+        //   { lat: currPos.value.lat+5, lng: currPos.value.lng-1.7 },
+        //   { lat: currPos.value.lat+10, lng: currPos.value.lng-2.3 }
+        // ];        
 
         const haversineDistance = (pos1, pos2) => {
         const R = 3958.8 // Radius of the Earth in miles
@@ -190,6 +356,12 @@ import { Loader } from '@googlemaps/js-api-loader'
         )
         return { currPos, otherLoc, distance, mapDivHere, calculatedTime}
       },
+      methods: {
+        addWaypoint() {
+
+
+        }
+      }
     
     }
   
@@ -234,6 +406,7 @@ import { Loader } from '@googlemaps/js-api-loader'
     <div class="submit-div" @click="$emit('someEvent', {c:currPos, d:otherLoc, distance:distance} )">CONFIRM</div>
     <div ref="mapDivHere" style="width:100%; height:80vh;"/>
     <div id="airportClicked"></div>
+    <div id="addWaypoint">Add Waypoint</div>
   </div>
 
 </template>
@@ -303,6 +476,27 @@ import { Loader } from '@googlemaps/js-api-loader'
     padding: 10px;
     border: solid 1.5px grey;
     border-radius: 20px;
+    opacity: .9;
     display: none;
+  }
+
+  #addWaypoint {
+    background-color: white;
+    color: black;
+    width: 19%;
+    position: absolute;
+    top: 35%;
+    margin-left: 1.2%;
+    text-align: center;
+    padding: 5px;
+    border: solid .1px #9a9a9a;
+    border-radius: 3px;;
+    box-shadow: 0px 1px 3px #576481;
+    display: none;
+  }
+
+  #addWaypoint:hover {
+    background-color: rgb(235, 234, 234);
+    cursor: pointer;
   }
 </style>
