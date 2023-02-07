4ab4ca6c7a929aaabedff7922a7818878e930ac9
Tom O <58118455+Tatooine-Soldier@users.noreply.github.com>
2023-02-07 15:29:55 +0000

    Graph adjacency list is iterable

--- graph.js
+++ graph.js
@@ -4,6 +4,7 @@
 
 export class Graph {
     constructor() {
+        this.nodesList = []
         this.adjacencyList = new Map(); //adjacency list contains keys are nodes and value is list of edges fron that node
     }
 
@@ -22,24 +23,59 @@ export class Graph {
         .then((response) => {
           const data = response.data;
           for (var val in data) {
-            console.log("data[val][coordinate]:", data[val]["coordinate"])
+            var coord =  {
+                id: data[val]["id"],
+                coordinate: data[val]["coordinate"]
+            }
+            // console.log(coord)
+            this.adjacencyList.set(coord, [])
+            this.nodesList.push(coord)
+            
           }
+          //console.log("this.adjacencyList:", this.adjacencyList)
+          for (let [key, val] of this.adjacencyList) {
+            let n = new Node()
+            n.value =  key
+            n.edges = val 
+            this.nodesList.push(n)
+
+          }
+          this.linkEdges()
+          //write function here that loops through the adjacency list and add edges to nodes that are less than .5 away from eachother
         })
         .catch (function (error) {
             console.log("ERROR:", error);    
         })
     }
-  
+    //for each node in the graph, create a edge between any node thats less than .5 away 
+    linkEdges() {
+        console.log("Calling link edges")
+        for (var nouter in this.nodesList) { 
+            console.log("nouter this.nodesList[nouter]", this.nodesList[nouter].coordinate.lat)
+            for (var ninner in this.nodesList) {
+                console.log("ninner this.nodesList[nouter]", this.nodesList[ninner].coordinate.lat)
+                // if ( ((this.nodesList[nouter].value.coordinate.lat - this.nodesList[ninner].value.coordinate.lat) < 0.06) || ((this.nodesList[nouter].value.coordinate.lng - this.nodesList[ninner].value.coordinate.lng) < 0.06) ) {
+                //     this.adjacencyList.set(this.nodesList[nouter].value, this.nodesList[nouter].edges.push(this.nodesList[ninner].value))  //put edge between niodes
+                //     console.log("added edge between ", this.nodesList[nouter].value, " and ", this.nodesList[ninner].value)
+                // } 
+            }
+        }
+    }
+
 }
 
 export class Node {
     constructor(element) {
         this.value = element
-        this.edges = {}
+        this.edges = []
     }
 
     getValue() {
         return this.value
+    }
+
+    getEdges() {
+        return this.edges
     }
 }
 
