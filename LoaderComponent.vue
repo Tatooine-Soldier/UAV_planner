02e5f77ba4fd5a4ab6721c0df0443f5c136bdaab
Tom O <58118455+Tatooine-Soldier@users.noreply.github.com>
2023-02-28 14:01:28 +0000

    Loader component

---
+++ LoaderComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="wavy">
+        <span>L</span><span>o</span><span>a</span><span>d</span><span>i</span><span>n</span><span>g</span><span>.</span><span>.</span><span>.</span>
+    </div>
+</template>
+
+<style>
+    .wavy {
+        top: 30%;
+        animation: waviy 2s infinite;
+    }
+
+    @keyframes waviy {
+        0%,40%,100% {
+          transform: translateY(0)
+        }
+        20% {
+          transform: translateY(-20px)
+        }
+      }
+</style>
