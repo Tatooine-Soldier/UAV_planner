27d2ed552f41cac473e234e9b50f82403f002301
Tom O <58118455+Tatooine-Soldier@users.noreply.github.com>
2023-03-18 17:55:34 +0000

    Add files via upload

---
+++ queue.go
@@ -0,0 +1,24 @@
+package main
+
+type Queue struct {
+	list            []QueueItem
+	GridID          string
+	SubGridAltitude string
+}
+
+type QueueItem struct {
+	FlightID string
+}
+
+func (q *Queue) Enqueue(item QueueItem) {
+	q.list = append(q.list, item)
+}
+
+func (q *Queue) Dequeue() interface{} {
+	if len(q.list) == 0 {
+		return nil
+	}
+	item := q.list[0]
+	q.list = q.list[1:]
+	return item
+}
