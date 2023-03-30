35d9174e7d5d513930ce335cf2c36e3deac62016
Tom O <58118455+Tatooine-Soldier@users.noreply.github.com>
2023-03-30 02:13:17 +0100

    Display the username of user logged in

---
+++ ProfileComponent.vue
@@ -0,0 +1,38 @@
+<template>
+    <section class="profile-page">
+        <section class="profile-main">
+            <p>Profile Page</p>
+            <h2>Welcome {{ user }}</h2>
+        </section>
+    </section>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            user: ""
+        }
+    },
+    async created() {
+        axios
+        .post("/userProfile")
+        .then((response) => {
+          const data = response.data;
+          console.log("data: ", data)
+          this.user = data
+        })
+        .catch(function (error) {
+            console.log("ERROR:", error); 
+        })
+    }
+}
+</script>
+
+<style>
+.profile-main {
+    color: white;
+}
+</style>
